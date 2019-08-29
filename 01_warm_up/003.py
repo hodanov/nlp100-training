b2c79d1fb8c6828c6b7e98b8a27ee2432f007cff
hodanov <1031hoda@gmail.com>
2019-08-29 13:16:54 +0900

    Modify 003

--- 01_warm_up/003.py
+++ 01_warm_up/003.py
@@ -1,12 +1,27 @@
 import re
 
 
-def split_words(words):
+def split_words(words: str) -> []:
+    """
+    単語を分割、リストにして返す。
+    """
     words = re.split(' |\\.|\\,', words)
     return words
+
+
+def count_char(words: []) -> []:
+    """
+    各単語の（アルファベットの）文字数を先頭から順に並べたリストを作成
+    """
+    list = []
+    for word in words:
+        if re.findall('[a-zA-Z]+', word):
+            list.append(len(word))
+    return list
 
 
 words = 'Now I need a drink, ' \
         'alcoholic of course, ' \
         'after the heavy lectures involving quantum mechanics.'
 print(split_words(words))
+print(count_char(split_words(words)))
